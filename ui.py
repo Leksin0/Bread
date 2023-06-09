7afca536bde5aa8b2679a46f28163bb9957dcd32
Leksin0 <90154033+Leksin0@users.noreply.github.com>
2023-06-09 21:22:04 +0300

    Add files via upload

--- ui.py
+++ ui.py
@@ -2,9 +2,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QFormLayout, QGr
 from PyQt6.QtWidgets import QPushButton, QLabel, QFileDialog, QCheckBox, QDateTimeEdit, QVBoxLayout
 from PyQt6.QtCore import QSize
 import sys
-
 import fileops
-
+import datetime
 
 #fixedsize ???
 
@@ -16,23 +15,36 @@ class MainWindow(QMainWindow):
         chdir = QPushButton()
         chdir.clicked.connect(self.chdir_click)
 
-
         self.cbl1 = QCheckBox("Линия 1")
         self.cbl2 = QCheckBox("Линия 2")
         self.cbl3 = QCheckBox("Линия 3")
         lineselect = QGroupBox()
         h = QVBoxLayout()
-        h.addWidget(cbl1)
-        h.addWidget(cbl2)
-        h.addWidget(cbl3)
+        h.addWidget(self.cbl1)
+        h.addWidget(self.cbl2)
+        h.addWidget(self.cbl3)
         lineselect.setLayout(h)
         lineselect.setAlignment(1)# align left
 
-        btresult = QPushButton()
-        result.clicked.connect(self.GetResult)
+        lbts = QLabel("Начало периода")
+        lbts.setAlignment(1)
+        lbte = QLabel("Конец периода")
+        lbte.setAlignment(1)
+        self.timestart = QDateTimeEdit(datetime.datetime.now())
+        self.timestart.setAlignment(2)
+        self.timeend = QDateTimeEdit(datetime.datetime.now())
+        self.timeend.setAlignment(2)
+
+        btresult = QPushButton("Сохранить")
+        btresult.clicked.connect(self.GetResult)
+        btchdir = QPushButton("Изменить папку логов")
+        btchdir.clicked.connect(self.chdir_click)
 
         layout = QFormLayout()
-        #TODO
+        layout.addRow(lbts, self.timestart)
+        layout.addRow(lbte, self.timeend)
+        layout.addWidget(lineselect)
+        layout.addRow(btresult, btchdir)
 
         self.setWindowTitle("Статистика производства хлеба")
         container = QWidget()
@@ -52,22 +64,20 @@ class MainWindow(QMainWindow):
             lines.append('2')
         if self.cbl3.isChecked():
             lines.append('3')
-        dtms =
-        fileops.Result(lines, dtms, dtme)
+        #dtms =  TODO
+        #fileops.Result(lines, dtms, dtme)
 
 
-class DirectoryChooseWindow(QWidget):
+class DirectoryChooseWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         btnl1 = QPushButton("Выбрать папку линии 1")
         btnl1.clicked.connect(self.btnl1_click)
         self.lbl1 = QLabel("")
-
         btnl2 = QPushButton("Выбрать папку линии 2")
         btnl2.clicked.connect(self.btnl2_click)
         self.lbl2 = QLabel("")
-
         btnl3 = QPushButton("Выбрать папку линии 3")
         btnl3.clicked.connect(self.btnl3_click)
         self.lbl3 = QLabel("")
@@ -82,7 +92,13 @@ class DirectoryChooseWindow(QWidget):
         layout.addRow(btnl2, self.lbl2)
         layout.addRow(btnl3, self.lbl3)
         layout.addRow(btnok, btncancel)
-        self.setLayout(layout)
+        #self.setLayout(layout)
+
+        container = QWidget()
+        container.setLayout(layout)
+        container.setFixedSize(500, 300)
+        self.setCentralWidget(container)
+        self.setWindowTitle("ГДЕ ХЛЕБ ?!!??")
 
     def btnl1_click(self):
         res = QFileDialog.getExistingDirectory(self)
@@ -104,6 +120,7 @@ class DirectoryChooseWindow(QWidget):
         return
 
     def cancel_click(self):
+        self.close()
         return
 
 
